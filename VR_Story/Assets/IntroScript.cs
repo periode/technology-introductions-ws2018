a1d05e0968cb5d1d7200e6caa00167d91b09d895
Pierre Depaz <pierre.depaz@gmail.com>
2018-11-22 11:18:31 +0100

    added vr story example

---
+++ VR_Story/Assets/IntroScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log(GvrControllerInput.ClickButtonDown);
+	}
+
+    public void OnPointedSphere(){
+        Debug.Log("pointed");
+    }
+
+    public void OnClickedSphere()
+    {
+        Debug.Log("click");
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ VR_Story/Assets/SphereManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnPointerEnteredMySphere(){
+        Debug.Log("enter");
+    }
+
+    public void OnPointerExited(string isTrue){
+        Debug.Log("exited");
+        for (int i = 0; i < 100; i++)
+        {
+            Debug.Log("counting");
+        }
+    }
+}
